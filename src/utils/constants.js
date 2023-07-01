f7570718055c1813c104e2bb5e42a3d82dcce01d
Suhas Khulpe <suhaskhulpe146@gmail.com>
2023-07-02 02:48:52 +0530

    updated secret key

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = 'AIzaSyAxHte-y0IsFPtxj9gPASaUO6mJehgPnEc';
+const GOOGLE_API_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXX';
 export const YOUTUBE_VIDEO_API = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=' + GOOGLE_API_KEY;
 export const YOUTUBE_VIDEO_WATCH_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&key=${GOOGLE_API_KEY}&id=`;
 export const YOUTUBE_RECOMENDED_VIDEO_LIST_API = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=50&key=${GOOGLE_API_KEY}&relatedToVideoId=`;
