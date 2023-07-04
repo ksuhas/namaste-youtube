0bb6242feab9b83b0f5c3e84532fcf064e94446a
Suhas Khulpe <suhaskhulpe146@gmail.com>
2023-07-04 23:53:55 +0530

    Live Stream chatting feature added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,3 +4,4 @@ export const YOUTUBE_VIDEO_WATCH_API = `https://youtube.googleapis.com/youtube/v
 export const YOUTUBE_RECOMENDED_VIDEO_LIST_API = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=50&key=${GOOGLE_API_KEY}&relatedToVideoId=`;
 export const YOUTUBE_SEARCH_VIDEO_WITH_QUERY_API = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&key=${GOOGLE_API_KEY}&q=`;
 export const YOUTUBE_SEARCH_VIDEO_API = `http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=`;
+export const OFFSET_LIVE_CHAT = 25;
